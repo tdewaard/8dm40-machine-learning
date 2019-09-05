4b9efd58a7fd2fa4f55cc33076a197069e23d523
Tristan de Waard <trist4nd4cunh4@gmail.com>
2019-09-05 17:39:39 +0200

    Added linear regression evaluation

--- practicals/code/linear_regression.py
+++ practicals/code/linear_regression.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def lsq(X, y):
     """
     Least squares linear regression
@@ -16,3 +17,18 @@ def lsq(X, y):
     beta = np.dot(np.linalg.inv(np.dot(X.T, X)), np.dot(X.T, y))
 
     return beta
+
+
+def eval_lin_model(beta, X_train, y_train):
+    """
+    Evaluates the linear model with coefficient vector beta on training data,
+    by calculating the mean squared error
+    :param beta: coefficient vector
+    :param X: the training data X vector/matrix (attribute values)
+    :param y_train: the training data Y vector (output, class variable)
+    :return: MSE: Mean Squared Error of the model
+    """
+    w = beta.T[:, 1:]
+    y = beta.T[:, 0] + np.dot(X_train, w.T)
+    MSE = np.sum((y - y_train)**2)/y_train.shape[0]
+    return MSE
