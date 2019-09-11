bb21b8419fc117b86180ea82321d9d00ae7b241d
Asch <t.g.v.asch@student.tue.nl>
2019-09-11 11:51:18 +0200

    Adjustment NN-regression

--- practicals/code/nn_regression_functions.py
+++ practicals/code/nn_regression_functions.py
@@ -9,13 +9,28 @@ Excercise k-NN Classification - Week 1
 #Import all functionalities and the dataset
 import numpy as np
 
-def k_NN_Regression(dataset,features,k):
+def k_NN_Regression(dataset,k):
+    
+    """
+    dataset = The combination of training and test datasets
+    k = The desired amount of neighbours used to estimate the value
+    
+    Appoint the dataset to a variable value and devide the dataset into a training and testing subdataset.
+    """
+    
     # Split data in train and test data
     X_train = dataset.data[:300, :]
     y_train = dataset.target[:300, np.newaxis]
     X_test = dataset.data[300:, :]
     y_test = dataset.target[300:, np.newaxis]
 
+    
+    """
+    The results are stored in the variable 'results'. In the for loop, the distances between the sample from the 
+    test data, and all of the training data is calculated and stored in the the list diff. After sorting this list, the k nearest 
+    neighbours (with minimal distance to the sample) were evaluated and the corresponding targets were used to estimate the test value.
+    """
+    
     #Compare length for each sample in train dataset and determine nearest neighbour
     results = np.zeros(len(X_test))
     
@@ -38,6 +53,12 @@ def k_NN_Regression(dataset,features,k):
         estimate = sum(targets)/k
         results[i] = estimate
     
+    
+    """
+      The Mean Squared Error (MSE) is calculated to evaluate the model. The MSE is defined by the difference between the 
+      original target value and the predicted target value, squared. 
+    """
+    
     #Determine MSE
     SE = np.zeros(len(y_test))
     for i in range(len(y_test)):
@@ -45,7 +66,7 @@ def k_NN_Regression(dataset,features,k):
     
     MSE = sum(SE) / len(SE)
     
-    return results, MSE, y_test, X_test
+    return results, MSE
             
         
         
