5a3fc187b9478c208f9cffa17911876fec9b6fe5
MartijnvanLeeuwen <54888999+MartijnvanLeeuwen@users.noreply.github.com>
2019-10-04 22:14:26 +0200

    update functions

--- practicals/code/change_brightness.py
+++ practicals/code/change_brightness.py
@@ -1,9 +1,17 @@
 def change_brightness(images):
+    
+    #input; (images): The input is a array of images 
+    #output: (alterdImages): The output is an array of images from which the brightness has been randomly scaled with a value between 0.1 and     2.0
+
     import random 
     import numpy as np
     alterdImages=[]
+    
     for i in range(len(images)):
+        #generate a random value between 0.1 and 2.0 
         random_value=random.randrange(10,200)/100
+        
+        #multiply the entire image with the randomly generated value in order to scale the intensity
         changed_brightness=images[i]*random_value
         alterdImages.append(changed_brightness)
     
--- practicals/code/gryds_function.py
+++ practicals/code/gryds_function.py
@@ -1,24 +1,31 @@
 def gryds_function(images):
+    # input: (images): The input is an array of images  
+    # output: (new_image): The output is a  geometricly augmented array of images with a randomly defined deformation/ rotation.
+    
     import numpy as np
     import gryds
+    
     new_image=[]
     
+    # A unique geomatrical augmentation is generated and applied to each image from the input
     for i in range(len(images)):
-
-
+        
+        # Defining the rotation operator
         affine = gryds.AffineTransformation(
                      ndim=2,
                      angles=[np.pi/4.], 
                      center=[0.5, 0.5]  
                      )
         
+        # Defining the different deformation operator
         random_grid = np.random.rand(2, 3, 3)
         random_grid -= 0.5
         random_grid /=10
-        
+       
         bspline = gryds.BSplineTransformation(random_grid) 
-
         interpolator = gryds.MultiChannelInterpolator(images[i], order=0, cval=[.1, .2, .3],mode='nearest')
+        
+        # Apply the deformation matrices to the input images
         transformed_image = interpolator.transform(bspline,affine)
         new_image.append(transformed_image)
         
